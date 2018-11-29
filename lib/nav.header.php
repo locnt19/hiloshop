602941cc3d36d35b9b92847c86a869a3844bda63
Nguyễn Thành Lộc <locnt19@gmail.com>
2018-11-30 01:05:22 +0700

    fix navigation

--- lib/nav.header.php
+++ lib/nav.header.php
@@ -15,35 +15,38 @@
 							<li><a href="index.php">trang chủ</a></li>
 							<li><a href="categories.php">sản phẩm</a></li>
 							<li>
-								<a href="single.php">
-								<span>giảm giá<sup class="text-danger"><strong>hot</strong></sup></span>
+								<a href="checkout.php">
+								<span>thanh toán</span>
 								</a>
 							</li>
 							<li><a href="contact.php">liên hệ</a></li>
 						</ul>
 						<ul class="navbar_user">
-							<li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
-							<li><a href="login.php"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+							<!-- <li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li> -->
 							<li class="checkout">
 								<a data-toggle="modal" data-target="#cart" style="cursor: pointer">
 								<i class="fa fa-shopping-cart" aria-hidden="true"></i>
 								<span id="checkout_items" class="checkout_items">19</span>
 								</a>
 							</li>
+							<li><a href="login.php"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+							<?php
+								if(isset($_SESSION['user'])) {
+								?>
+							<li style="margin: 0 1rem">
+								<a href="#"><?=$_SESSION['user']['TenTK']?></a>
+							</li>
 							<li>
-								<?php
-									if(isset($_SESSION['user'])) {
-										?>
-										<a href="#"><?=$_SESSION['user']['TenTK']?></a>
-										<a href="login.php">Logout</a>
-										<?php
-									}
-									else {
-										?>
-										<a href="login.php">Đăng nhập</a>
-										<?php
-									}
+								<a href="login.php">Logout</a>
+							</li>
+							<?php
+								}
+								else {
 									?>
+									<a href="login.php">Đăng nhập</a>
+							<?php
+								}
+								?>
 							
 							</li>
 						</ul>
@@ -64,20 +67,31 @@
 	<div class="hamburger_menu_content text-right">
 		<ul class="menu_top_nav">
 			<li class="menu_item has-children">
-				<a href="#">My Account<i class="fa fa-angle-down"></i></a>
+				<a href="#">Tài khoản<i class="fa fa-angle-down"></i></a>
 				<ul class="menu_selection">
-					<li><a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i>&nbsp;đăng nhập</a></li>
-					<li><a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i>&nbsp;đăng ký</a></li>
+				<?php
+					if(!isset($_SESSION['user'])) {
+					?>
+					<li><a href="login.php"><i class="fa fa-sign-in" aria-hidden="true"></i>&nbsp;đăng nhập</a></li>
+					<li><a href="register.php"><i class="fa fa-user-plus" aria-hidden="true"></i>&nbsp;đăng ký</a></li>
+				<?php
+					} else {
+					?>
+					<li><a href="#"><i class="fa fa-user" aria-hidden="true"></i>&nbsp;<?=$_SESSION['user']['TenTK']?></a></li>
+					<li><a href="#"><i class="fa fa-sign-out" aria-hidden="true"></i>&nbsp;đăng xuất</a></li>
+				<?php
+					} 
+					?>
 				</ul>
 			</li>
-			<li class="menu_item"><a href="index.html">trang chủ</a></li>
-			<li class="menu_item"><a href="categories.html">sản phẩm</a></li>
+			<li class="menu_item"><a href="index.php">trang chủ</a></li>
+			<li class="menu_item"><a href="categories.php">sản phẩm</a></li>
 			<li class="menu_item">
-				<a href="single.html">
+				<a href="single.php">
 				<span>giảm giá<sup class="text-danger"><strong>hot</strong></sup></span>
 				</a>
 			</li>
-			<li class="menu_item"><a href="contact.html">liên hệ</a></li>
+			<li class="menu_item"><a href="contact.php">liên hệ</a></li>
 		</ul>
 	</div>
 </div>
