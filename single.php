a4fcc07b233ad50bd458783f79a40062a2353a19
Nguyễn Thành Lộc <locnt19@gmail.com>
2018-12-02 02:01:33 +0700

    add product from single

--- single.php
+++ single.php
@@ -1,6 +1,21 @@
 <?php
 	session_start();
-    ?>
+	if(!isset($_SESSION['gio_hang'])) {
+		$_SESSION['gio_hang'] = array();
+	}
+	// if(isset($count)) {
+	// 	$_SESSION['tong_so_luong'] += $count;
+	// 	$count = 0;
+	// } else {
+	// 	$count = 0;
+	// }
+	if(isset($_GET["MaSP"])) {
+		$maSP = $_GET["MaSP"];
+		include ("lib/connection.php");
+		$sql = "select * from san_pham where MaSP='$maSP'";
+		$result = $conn->query($sql);
+	}
+	   ?>
 <!DOCTYPE html>
 <html lang="vi">
 	<head>
@@ -14,88 +29,84 @@
 			include ("lib/nav.header.php");
 			?>
 		<div class="container single_product_container">
-			<div class="row">
-				<div class="col">
-					<!-- Breadcrumbs -->
-					<div class="breadcrumbs d-flex flex-row align-items-center">
-						<ul>
-							<li><a href="index.php">Trang chủ</a></li>
-							<?php 
-								if(isset($_GET["MaSP"])) {
-									$maSP = $_GET["MaSP"];
-									include ("lib/connection.php");
-									// $sql = "SELECT * FROM san_pham WHERE MaSP=$maSP";
-									$sql = "select * from san_pham where MaSP='$maSP'";
-									$result = $conn->query($sql);
+			<form method="get">
+				<div class="row">
+					<div class="col">
+						<!-- Breadcrumbs -->
+						<div class="breadcrumbs d-flex flex-row align-items-center">
+							<ul>
+								<li><a href="index.php">Trang chủ</a></li>
+								<?php
 									if($result->num_rows > 0) {
 										$row = $result->fetch_assoc();
-								?>
-							<li><a href="categories.php"><i class="fa fa-angle-right" aria-hidden="true"></i><?=$row["NSX"]?></a></li>
-							<li class="active"><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i><?=$row["TenSP"]?></a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-lg-7">
-					<div class="single_product_pics">
-						<div class="row">
-							<div class="col-lg-3 thumbnails_col order-lg-1 order-2">
-								<div class="single_product_thumbnails">
-									<ul>
-										<li class="active"><img src="<?=$row["HinhAnh"]?>" alt="" data-image="<?=$row["HinhAnh"]?>"></li>
-										<li><img src="<?=$row["HinhAnh"]?>" alt="" data-image="<?=$row["HinhAnh"]?>"></li>
-										<li><img src="<?=$row["HinhAnh"]?>" alt="" data-image="<?=$row["HinhAnh"]?>"></li>
-									</ul>
-								</div>
-							</div>
-							<div class="col-lg-9 image_col order-lg-2 order-1">
-								<div class="single_product_image">
-									<div class="single_product_image_background" style="background-image:url(<?=$row["HinhAnh"]?>)"></div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-5">
-					<div class="product_details">
-						<div class="product_details_title">
-							<h2><?=$row["TenSP"]?></h2>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque, amet soluta alias dicta nemo ratione quam voluptatum accusantium quaerat vitae natus dolorum voluptates quos perspiciatis cupiditate aliquid porro dignissimos?</p>
-						</div>
-						<div class="free_delivery d-flex flex-row align-items-center justify-content-center">
-							<span class="ti-truck"></span><span>giao hàng miễn phí</span>
-						</div>
-						<!-- <div class="original_price">$629.99</div> -->
-						<div class="product_price"><?=$row["DonGia"]?> VNĐ</div>
-						<ul class="star_rating">
-							<li><i class="fa fa-star" aria-hidden="true"></i></li>
-							<li><i class="fa fa-star" aria-hidden="true"></i></li>
-							<li><i class="fa fa-star" aria-hidden="true"></i></li>
-							<li><i class="fa fa-star" aria-hidden="true"></i></li>
-							<li><i class="fa fa-star" aria-hidden="true"></i></li>
-						</ul>
-						<div class="product_color">
-							<span>Chọn màu sắc:</span>
-							<ul>
-								<li style="background: #e54e5d"></li>
-								<li style="background: #252525"></li>
-								<li style="background: #60b3f3"></li>
+									?>
+								<li><a href="categories.php"><i class="fa fa-angle-right" aria-hidden="true"></i><?=$row["NSX"]?></a></li>
+								<li class="active"><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i><?=$row["TenSP"]?></a></li>
 							</ul>
 						</div>
-						<div class="quantity d-flex flex-column flex-sm-row align-items-sm-center">
-							<span>Số lượng:</span>
-							<div class="quantity_selector">
-								<span class="minus"><i class="fa fa-minus" aria-hidden="true"></i></span>
-								<span id="quantity_value">1</span>
-								<span class="plus"><i class="fa fa-plus" aria-hidden="true"></i></span>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-7">
+						<div class="single_product_pics">
+							<div class="row">
+								<div class="col-lg-3 thumbnails_col order-lg-1 order-2">
+									<div class="single_product_thumbnails">
+										<ul>
+											<li class="active"><img src="<?=$row["HinhAnh"]?>" alt="" data-image="<?=$row["HinhAnh"]?>"></li>
+											<li><img src="<?=$row["HinhAnh"]?>" alt="" data-image="<?=$row["HinhAnh"]?>"></li>
+											<li><img src="<?=$row["HinhAnh"]?>" alt="" data-image="<?=$row["HinhAnh"]?>"></li>
+										</ul>
+									</div>
+								</div>
+								<div class="col-lg-9 image_col order-lg-2 order-1">
+									<div class="single_product_image">
+										<div class="single_product_image_background" style="background-image:url(<?=$row["HinhAnh"]?>)"></div>
+									</div>
+								</div>
 							</div>
-							<div class="red_button add_to_cart_button"><a href="#">Thêm vào giỏ hàng</a></div>
-							<div class="product_favorite d-flex flex-column align-items-center justify-content-center"></div>
+						</div>
+					</div>
+					<div class="col-lg-5">
+						<div class="product_details">
+							<div class="product_details_title">
+								<h2><?=$row["TenSP"]?></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque, amet soluta alias dicta nemo ratione quam voluptatum accusantium quaerat vitae natus dolorum voluptates quos perspiciatis cupiditate aliquid porro dignissimos?</p>
+							</div>
+							<div class="free_delivery d-flex flex-row align-items-center justify-content-center">
+								<span class="ti-truck"></span><span>giao hàng miễn phí</span>
+							</div>
+							<div class="product_price"><?=number_format($row["DonGia"])?> VNĐ</div>
+							<ul class="star_rating">
+								<li><i class="fa fa-star" aria-hidden="true"></i></li>
+								<li><i class="fa fa-star" aria-hidden="true"></i></li>
+								<li><i class="fa fa-star" aria-hidden="true"></i></li>
+								<li><i class="fa fa-star" aria-hidden="true"></i></li>
+								<li><i class="fa fa-star" aria-hidden="true"></i></li>
+							</ul>
+							<div class="product_color">
+								<span>Chọn màu sắc:</span>
+								<ul>
+									<li style="background: #e54e5d"></li>
+									<li style="background: #252525"></li>
+									<li style="background: #60b3f3"></li>
+								</ul>
+							</div>
+							<div class="quantity d-flex flex-column flex-sm-row align-items-sm-center">
+								<span>Số lượng:</span>
+								<div class="quantity_selector">
+									<span class="minus"><i class="fa fa-minus" aria-hidden="true"></i></span>
+									<input type="text" readonly style="border: none; width: 20px" id="quantity_value" name="them" value="1">
+									<span class="plus"><i class="fa fa-plus" aria-hidden="true"></i></span>
+								</div>
+								<div class="product_favorite d-flex flex-column align-items-center justify-content-center"></div>
+							</div>
+							<input type="hidden" name="MaSP" value="<?=$row['MaSP']?>">
+							<input type="submit" style="border: none; color: #fff; font-weight: bold" class="red_button shop_now_button" value="Thêm vào giỏ hàng"/>
 						</div>
 					</div>
 				</div>
-			</div>
+			</form>
 		</div>
 		<!-- Tabs -->
 		<div class="tabs_section_container">
@@ -147,7 +158,6 @@
 						</div>
 						<?php
 							}
-							}
 							?>
 						<!-- Tab Additional Info -->
 						<div id="tab_2" class="tab_container">
@@ -166,6 +176,14 @@
 			</div>
 		</div>
 		<?php
+			if(isset($_GET['them']) && isset($_GET['MaSP'])) {
+				$row['SoLuong'] = (int)($_GET['them']);
+				array_push($_SESSION['gio_hang'],$row);
+				$_SESSION['tong_so_luong']+= $row['SoLuong'];
+				// $count += $row['SoLuong'];
+				// var_dump($count);
+				echo "<script>alert('Thêm thành công')</script>";
+			}
 			include ("lib/footer.master.php");
 			include ("lib/js.single.php");
 			?>
